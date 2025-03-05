643dc90d8478d6ef77e378105fac8254b66c5ad1
simon <simon.renard@ulb.be>
2025-03-05 15:33:14 +0100

    Change scoring methods

--- python/utils.py
+++ python/utils.py
@@ -1,6 +1,8 @@
 import multiprocessing
 import operator
 import random
+import numpy as np
+
 
 import dill as pickle
 import tempfile
@@ -18,22 +20,34 @@ from functor_observer import FunctorObserver
 
 
 
+def shifted_geometric_mean(values, shift=1.0):
+    values = np.array(values)
+    geom_mean = np.exp(np.mean(np.log(values + shift))) - shift
+    return geom_mean
+
 def solve(path, individual, feature_observer, scip_params):
     toolbox = create_tool_box()
     func = toolbox.compile(expr=individual)
     # solver = solvers.EcoleSolver(FunctorObserver(func, MyObserver()), scip_params=scip_params)
     solver = solvers.EcoleSolver(FunctorObserver(func, feature_observer), scip_params=scip_params)
     solver.solve(path)
-    return solver["estimate_nnodes"]
+    return solver["estimate_nnodes"], solver["nnodes"], solver["time"]
 
-def evalSymbReg(individual, pool: multiprocessing.Pool, instances_path, baseline_nodes, *args):
+def evalSymbReg(individual, pool: multiprocessing.Pool, instances_path, baseline_average_time, *args):
     async_results = [pool.apply_async(solve, args=(p, individual, *args)) for p in instances_path]
     def get_res():
-        nnodes = [ar.get() for ar in async_results]
-        for i in range(len(nnodes)):
-            nnodes[i] /= baseline_nodes[i]
+        # score = 0
+        estimate_times = []
+        for estimate_nnodes, nnodes, time in [ar.get() for ar in async_results]:
+            estimate_time = estimate_nnodes * (time/nnodes)
+            if estimate_time < time:
+                estimate_time = 10*time
+            estimate_times.append(estimate_time)
 
-        return sum(nnodes) / len(nnodes) + individual.height**0.5/100,
+            # print(nnodes, estimate_nnodes, time, estimate_time)
+            # score += max(estimate_time, time)
+
+        return shifted_geometric_mean(estimate_times, 1)/baseline_average_time, # * (1 + individual.height/100),
 
     return get_res
 
@@ -49,10 +63,20 @@ def evalSymbReg(individual, pool: multiprocessing.Pool, instances_path, baseline
 
 
 def mapp(f, individuals, args):
+    args = args[:]
     callbacks = []
+    instances, nnodes_baseline, n = args.pop(0)
+
+    if n is not None:
+        indexes = random.choices(range(len(instances)), k=n)
+        instances = [instances[i] for i in indexes]
+        nnodes_baseline = [nnodes_baseline[i] for i in indexes]
+
+    baseline_average_time = shifted_geometric_mean(nnodes_baseline, 1)
+
     with Pool() as pool:
         for individual in individuals:
-            callbacks.append(f(individual, pool, *args))
+            callbacks.append(f(individual, pool, instances, baseline_average_time, *args))
 
         pool.close()
         pool.join()
@@ -60,7 +84,7 @@ def mapp(f, individuals, args):
     result = [callback() for callback in callbacks]
 
     return result
-# Define new functions
+
 def protectedDiv(left, right):
     try:
         return left / right
@@ -74,14 +98,14 @@ def if_then_else(cond, then, default):
         return default
 
 
-def create_tool_box(observer = None, instances_paths = None, baseline_nodes = None, scip_params = None, name = "MAIN"):
+def create_tool_box(observer = None, instances_paths = None, scip_params = None, n_instance=None, nnodes_baseline = None):
     if not hasattr(create_tool_box, "toolbox"):
         create_tool_box.toolbox = None
 
     if observer is None:
         return create_tool_box.toolbox
 
-    pset = gp.PrimitiveSet(name, len(observer))
+    pset = gp.PrimitiveSet("MAIN", len(observer))
     # pset = gp.PrimitiveSet("MAIN", 72)
     pset.addPrimitive(operator.add, 2)
     pset.addPrimitive(operator.sub, 2)
@@ -91,7 +115,9 @@ def create_tool_box(observer = None, instances_paths = None, baseline_nodes = No
     pset.addPrimitive(operator.gt, 2)
     pset.addPrimitive(operator.neg, 1)
     pset.addPrimitive(if_then_else, 3)
-    pset.addTerminal(10000)
+
+    for i in range(-3, 5):
+        pset.addTerminal(2**i)
 
     creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
     creator.create("Individual", gp.PrimitiveTree, fitness=creator.FitnessMin)
@@ -112,9 +138,9 @@ def create_tool_box(observer = None, instances_paths = None, baseline_nodes = No
 
     toolbox.register("evaluate", evalSymbReg)
 
-    assert (instances_paths is None and baseline_nodes is None and scip_params is None) or (instances_paths is not None and baseline_nodes is not None and scip_params is not None), "If one argument is provided, the 3 must be set"
+    assert (instances_paths is None and scip_params is None) or (instances_paths is not None and scip_params is not None), "If one argument is provided, the 3 must be set"
     if instances_paths is not None:
-        args = [instances_paths, baseline_nodes, observer, scip_params]
+        args = [(instances_paths, nnodes_baseline, n_instance), observer, scip_params]
         toolbox.register("map", mapp, args=args)
 
     create_tool_box.toolbox = toolbox
@@ -126,7 +152,7 @@ def solve_rb(path, scip_params={}):
     solver.solve(path)
     return solver["estimate_nnodes"]
 
-def train(observer, instances, pop_size, n_generations, best_individual_path="best_ind", scip_params = {}):
+def train(observer, instances, pop_size, n_generations, best_individual_path="best_ind", scip_params = {}, n_instances=None):
     instances_path = []
     files = []
 
@@ -139,11 +165,11 @@ def train(observer, instances, pop_size, n_generations, best_individual_path="be
         files.append(prob_file)
 
     with Pool() as pool:
-        baseline_node = pool.map(solve_rb, instances_path)
+        nnodes_baseline = pool.map(solve_rb, instances_path)
 
     print("Starting evolution...")
 
-    toolbox = create_tool_box(observer, instances_path, baseline_node, scip_params)
+    toolbox = create_tool_box(observer, instances_path, scip_params, n_instances, nnodes_baseline)
 
     pop = toolbox.population(n=pop_size)
     hof = tools.HallOfFame(1)
@@ -158,10 +184,11 @@ def train(observer, instances, pop_size, n_generations, best_individual_path="be
 
     for i in range(n_generations):
         print(i)
-        pop, log = algorithms.eaSimple(pop, toolbox, 0.5, 0.1, 1, stats=mstats,
+        pop, log = algorithms.eaSimple(pop, toolbox, 0.5, 0.3, 1, stats=mstats,
                                        halloffame=hof, verbose=True)
 
-        best = min(pop, key=lambda ind: ind.fitness.values[0])
+        # best = min(pop, key=lambda ind: ind.fitness.values[0])
+        best = hof.items[0]
         pickle.dump(best, open(best_individual_path, "wb"))
 
     print(best)
