a27dcdb989a186e3b00bdedaa2866bec2ebbb38f
Simon Renard <simon.renard@ulb.be>
2025-03-26 11:40:30 +0100

    Code to automatically generate scip branching rule in C++ from the individuals

---
+++ observer_generation/template.cpp
@@ -0,0 +1,136 @@
+#include <iostream>
+#include <map>
+#include <objscip/objbranchrule.h>
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <scip/scip.h>
+
+#include "../src/DynamicFeaturesObs.h"
+#include "../src/StaticFeaturesObs.h"
+#include "../src/TreeFeaturesObs.h"
+
+#define FORMULA 0
+
+namespace py = pybind11;
+
+class FeaturesWrapper {
+  StaticFeaturesObs &staticFeatures;
+  TreeFeaturesObs &treeFeatures;
+  DynamicFeaturesObs &dynamicFeatures;
+
+public:
+  FeaturesWrapper(StaticFeaturesObs &staticFeatures,
+                  TreeFeaturesObs &treeFeatures,
+                  DynamicFeaturesObs &dynamicFeatures)
+      : staticFeatures(staticFeatures), treeFeatures(treeFeatures),
+        dynamicFeatures(dynamicFeatures) {}
+
+  double operator[](int index) {
+    if (index < staticFeatures.size) {
+      return staticFeatures[index];
+    }
+
+    index -= staticFeatures.size;
+    if (index < treeFeatures.size) {
+      return treeFeatures[index];
+    }
+
+    index -= treeFeatures.size;
+    if (index < dynamicFeatures.size) {
+      return dynamicFeatures[index];
+    }
+
+    throw std::out_of_range("index out of range");
+  }
+};
+
+class template_name : public scip::ObjBranchrule {
+  std::map<int, DynamicFeaturesObs> dynamic_features;
+  std::map<int, StaticFeaturesObs> static_features;
+  std::unique_ptr<TreeFeaturesObs> tree_features;
+
+public:
+  template_name(SCIP *scip)
+      : ObjBranchrule(scip, "template_name", "Automatically generated", 0, -1,
+                      1) {}
+
+  SCIP_DECL_BRANCHINITSOL(scip_initsol) override {
+    tree_features =
+        std::make_unique<TreeFeaturesObs>(reinterpret_cast<long>(scip));
+    return SCIP_OKAY;
+  }
+
+  SCIP_DECL_BRANCHEXECLP(scip_execlp) override {
+    SCIP_VAR **lpcands;
+    int nlpcands;
+
+    /* get branching candidates */
+    SCIP_CALL(SCIPgetLPBranchCands(scip, &lpcands, NULL, NULL, NULL, &nlpcands,
+                                   NULL));
+
+    int bestcand = 0;
+    SCIP_Real bestScore = SCIP_REAL_MIN;
+
+    tree_features->reset();
+
+    for (int i = 0; i < nlpcands; i++) {
+      auto cand = lpcands[i];
+
+      int prob_index = SCIPvarGetProbindex(cand);
+      if (!dynamic_features.contains(prob_index)) {
+        dynamic_features.insert(std::make_pair(
+            prob_index, DynamicFeaturesObs(reinterpret_cast<long>(scip))));
+      }
+      if (!static_features.contains(prob_index)) {
+        static_features.insert(std::make_pair(
+            prob_index, StaticFeaturesObs(reinterpret_cast<long>(scip))));
+      }
+
+      auto dynamic_feature = dynamic_features.at(prob_index);
+      auto static_feature = static_features.at(prob_index);
+
+      dynamic_feature.reset();
+
+      dynamic_feature.setVar(prob_index);
+      static_feature.setVar(prob_index);
+
+      FeaturesWrapper features(static_feature, *tree_features,
+                               dynamic_feature);
+
+      SCIP_Real score = FORMULA;
+      if (score > bestScore) {
+        bestScore = score;
+        bestcand = i;
+      }
+    }
+
+    SCIP_CALL(SCIPbranchVar(scip, lpcands[bestcand], NULL, NULL, NULL));
+
+    *result = SCIP_BRANCHED;
+    return SCIP_OKAY;
+  }
+
+  SCIP_DECL_BRANCHEXITSOL(scip_exitsol) override {
+    dynamic_features.clear();
+    static_features.clear();
+    return SCIP_OKAY;
+  }
+};
+
+/** Creates and adds the custom branching rule to SCIP */
+void add_branching(py::object py_scip) {
+  // Extract SCIP* from PyCapsule
+  void *scip_ptr = PyCapsule_GetPointer(py_scip.ptr(), "scip");
+  if (!scip_ptr) {
+    throw py::error_already_set();
+  }
+
+  SCIP *scip = static_cast<SCIP *>(scip_ptr);
+
+  SCIPincludeObjBranchrule(scip, new template_name(scip), TRUE);
+}
+
+PYBIND11_MODULE(template_name, m) {
+  m.def("add_branching", &add_branching,
+        "Adds custom branching rule to SCIP");
+}
---
+++ python/generator.py
@@ -0,0 +1,99 @@
+import os
+
+import dill as pickle
+from observer import MyObserver
+from utils import create_tool_box
+
+class Generator:
+    def __init__(self, formula):
+        self.formula = formula
+
+    def extract_functions(self, formula):
+        function = ""
+        args = []
+        current_arg = ""
+        count = 0
+        for c in formula:
+            if c == "(":
+                count += 1
+                if count == 1:
+                    continue
+            elif c == ")":
+                count -= 1
+
+            if count == 0 and c == ")" or count == 1 and c == ",":
+                current_arg = current_arg.replace(" ", "")
+                args.append(current_arg)
+                current_arg = ""
+                continue
+
+            if count == 0:
+                if c != ")":
+                    function += c
+            else:
+                current_arg += c
+
+        return function, args
+
+    def get_function_code(self, function, args):
+        operators = {
+            "add": "+",
+            "sub": "-",
+            "mul": "*",
+            "lt": "<",
+            "gt": ">",
+            "and_": "&&",
+            "or_": "||",
+        }
+        if function in operators:
+            return f"({args[0]}) {operators[function]} ({args[1]})"
+
+        if function == "div":
+            return f"({args[1]}) == 0 ? 1 : ({args[0]})/({args[1]})"
+
+        if function == "not":
+            return f"!({args[0]})"
+
+        if function == "neg":
+            return f"-({args[0]})"
+
+        if function == "round":
+            return f"round({args[0]})"
+
+        return f"std::{function}({args[0]}, {args[1]})"
+
+
+
+    def parse(self, expr: str):
+        if expr.startswith("ARG"):
+            index = expr[3:]
+            code = f"features[{index}]"
+        else:
+            function, args = self.extract_functions(expr)
+            args = [self.parse(arg) for arg in args]
+            code = self.get_function_code(function, args)
+
+        return code
+
+    def generate_file(self, name, path):
+        with open("observer_generation/template.cpp", "r") as f:
+            content = f.read()
+
+        content = content.replace("template_name", name)
+
+        code = self.parse(self.formula)
+        content = content.replace("#define FORMULA 0", f"#define FORMULA {code}")
+
+        p = os.path.join(path, f"{name}.cpp")
+        with open(p, "w") as f:
+            f.write(content)
+
+
+if __name__ == "__main__":
+    name = "best_ca4_lazy"
+    observer = MyObserver()
+    toolbox, pset = create_tool_box(observer)
+    individial = pickle.load(open(f"individuals/{name}", "rb"))
+    print(individial)
+    gen = Generator(str(individial))
+    gen.generate_file(name, "observer_generation")
