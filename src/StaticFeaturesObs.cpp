6ee68113a746e6a4a47264d6b13d6d76ad62cc3a
Simon Renard <simon.renard@ulb.be>
2025-03-31 09:21:59 +0200

    Improve compilation

--- src/StaticFeaturesObs.cpp
+++ src/StaticFeaturesObs.cpp
@@ -1,5 +1,76 @@
-//
-// Created by simon on 14/02/25.
-//
-
 #include "StaticFeaturesObs.h"
+
+StaticFeaturesObs::StaticFeaturesObs(SCIP *scip): Obs(scip, size) {}
+
+StaticFeaturesObs::StaticFeaturesObs(py::object py_scip) : StaticFeaturesObs(
+        static_cast<SCIP *>(PyCapsule_GetPointer(py_scip.ptr(), "scip"))
+    ) {
+    if (!scip) {
+        throw py::error_already_set();
+    }
+}
+
+std::vector<double> StaticFeaturesObs::computeObjCoefficient() {
+    return {(SCIPvarGetObj(var))};
+}
+
+std::vector<double> StaticFeaturesObs::computeNonZeroCoefficientsStatistics() {
+    auto col = SCIPvarGetCol(var);
+    int count = SCIPcolGetNLPNonz(col);
+    auto data = ArrayView(SCIPcolGetVals(col), count);
+
+    auto positiveStats = statistics<ArrayView<double>, double>(data, [](double val) { return val > 0;; });
+    auto negativeStats = statistics<ArrayView<double>, double>(data, [](double val) { return val < 0; });
+
+    return {
+        static_cast<double>(count),
+        positiveStats.mean,
+        positiveStats.stdev,
+        positiveStats.min,
+        positiveStats.max,
+        negativeStats.mean,
+        negativeStats.stdev,
+        negativeStats.min,
+        negativeStats.max,
+    };
+}
+
+std::vector<double> StaticFeaturesObs::computeConstraintsDegreeStatistics() {
+    std::vector<double> degrees;
+    auto col = SCIPvarGetCol(var);
+    auto const n_rows = SCIPcolGetNNonz(col);
+    auto rows = SCIPcolGetRows(col);
+
+    for (int i = 0; i < n_rows; i++) {
+        auto row = rows[i];
+        degrees.push_back(SCIProwGetNNonz(row));
+    }
+
+    auto stats = statistics<std::vector<double>, double>(degrees);
+
+    return {
+        stats.mean,
+        stats.stdev,
+        stats.min,
+        stats.max,
+    };
+}
+
+void StaticFeaturesObs::compute(int index) {
+    std::vector<double> tmp;
+    int start = 0;
+    if (index < 1) {
+        tmp = computeObjCoefficient();
+    } else if (index < 10) {
+        start = 1;
+        tmp = computeNonZeroCoefficientsStatistics();
+    } else if (index < 14) {
+        start = 10;
+        tmp = computeConstraintsDegreeStatistics();
+    }
+
+    for (int i = 0; i < tmp.size(); i++) {
+        features[i + start] = tmp[i];
+        computed[i + start] = true;
+    }
+}
--- src/StaticFeaturesObs.h
+++ src/StaticFeaturesObs.h
@@ -1,125 +1,30 @@
-//
-// Created by simon on 14/02/25.
-//
-
 #ifndef STATICFEATURESOBS_H
 #define STATICFEATURESOBS_H
 
 #include <iostream>
 #include <vector>
-
 #include <scip/scip.h>
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
-namespace py = pybind11;
-
-
 #include "ArrayView.h"
 #include "Obs.h"
 #include "statistics.h"
 
+namespace py = pybind11;
+
 class StaticFeaturesObs : public Obs {
-    /**
-     * Get the variable objective's coefficient
-     * @return
-     */
-    std::vector<double> computeObjCoefficient() {
-        return {(SCIPvarGetObj(var))};
-    }
-
-    /**
-     * Statistics about variable's constraint coefficient. Report the number of constraint in which it is active
-     * And statistics on positive/negative coefficient (mean, stdev, min, max)
-     * @return vector of 9 features
-     */
-    std::vector<double> computeNonZeroCoefficientsStatistics() {
-        auto col = SCIPvarGetCol(var);
-
-        int count = SCIPcolGetNLPNonz(col);
-        auto data = ArrayView(SCIPcolGetVals(col), count);
-
-        auto positiveStats = statistics<ArrayView<double>, double>(data, [](double val) { return val > 0;; });
-        auto negativeStats = statistics<ArrayView<double>, double>(data, [](double val) { return val < 0; });
-
-        return {
-            static_cast<double>(count),
-            positiveStats.mean,
-            positiveStats.stdev,
-            positiveStats.min,
-            positiveStats.max,
-            negativeStats.mean,
-            negativeStats.stdev,
-            negativeStats.min,
-            negativeStats.max,
-        };
-    }
-
-    /**
-    * Stats. for constraint degrees.
-    *
-    * The degree of a constraint is the number of variables that participate in it.
-    * A variable may participate in multiple constraints, and statistics over those constraints'
-    * degrees are used. (mean, stdev, min, max)
-    * @return Vector of 4 features
-    */
-    std::vector<double> computeConstraintsDegreeStatistics() {
-        std::vector<double> degrees;
-
-        auto col = SCIPvarGetCol(var);
-        auto const n_rows = SCIPcolGetNNonz(col);
-        auto rows = SCIPcolGetRows(col);
-
-        for (int i = 0; i < n_rows; i++) {
-            auto row = rows[i];
-            degrees.push_back(SCIProwGetNNonz(row));
-        }
-
-        auto stats = statistics<std::vector<double>, double>(degrees);
-
-
-        return {
-            stats.mean,
-            stats.stdev,
-            stats.min,
-            stats.max,
-        };
-    }
-
-    void compute(int index) override {
-        std::vector<double> tmp;
-        int start = 0;
-        if (index < 1) {
-            tmp = computeObjCoefficient();
-        } else if (index < 10) {
-            start = 1;
-            tmp = computeNonZeroCoefficientsStatistics();
-        } else if (index < 14) {
-            start = 10;
-            tmp = computeConstraintsDegreeStatistics();
-        }
-
-
-        for (int i = 0; i < tmp.size(); i++) {
-            features[i + start] = tmp[i];
-            computed[i + start] = true;
-        }
-        // std::copy_backward(tmp.begin(), tmp.end(), features.begin()+start);
-        // std::fill(computed.begin() + start, computed.begin() + start + tmp.size(), true);
-    }
+private:
+    std::vector<double> computeObjCoefficient();
+    std::vector<double> computeNonZeroCoefficientsStatistics();
+    std::vector<double> computeConstraintsDegreeStatistics();
 
 public:
     static const int size = 14;
-
-    explicit StaticFeaturesObs(SCIP *scip): Obs(scip, size) {
-    }
-    explicit StaticFeaturesObs(py::object py_scip) : StaticFeaturesObs(
-        static_cast<SCIP *>(PyCapsule_GetPointer(py_scip.ptr(), "scip"))
-    ) {
-        if (!scip) {
-            throw py::error_already_set();
-        }
-    }
+    
+    explicit StaticFeaturesObs(SCIP *scip);
+    explicit StaticFeaturesObs(py::object py_scip);
+    
+    void compute(int index) override;
 };
 
-
-#endif //STATICFEATURESOBS_H
+#endif // STATICFEATURESOBS_H
