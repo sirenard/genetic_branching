5bc8ed40e815c6dfd1135edbb802513829527d01
Simon Renard <simon.renard@ulb.be>
2025-04-03 15:52:40 +0200

    Clean train/evaluate

--- python/evaluate.py
+++ python/evaluate.py
@@ -1,27 +1,47 @@
 import pickle
 
 import ecole
-from boundml.evaluation_tools import evaluate_solvers
+from boundml.evaluation_tools import evaluate_solvers, SolverEvaluationResults
 from boundml.solvers import ClassicSolver
 
 from ClassicSolverCustomBranching import ClassicSolverCustomBranching
 
-n_instances = 15
+n_instances = 10
 
 if __name__ == '__main__':
     instances = ecole.instance.CombinatorialAuctionGenerator(n_items=100, n_bids=500)
-    instances.seed(0)
+    # instances = ecole.instance.CapacitatedFacilityLocationGenerator(n_customers=100)
+    instances.seed(1212)
 
     scip_params = {
-        "limits/time": 200,
+        "limits/time": 300,
     }
+
+    custom_solvers = [
+        ClassicSolverCustomBranching("best_ca", scip_params),
+        ClassicSolverCustomBranching("best_ca_10_10", scip_params),
+        ClassicSolverCustomBranching("best_ca_10_30", scip_params),
+    ]
+
+    for solver in custom_solvers:
+        print(solver.get_expression())
 
     solvers = [
         ClassicSolver("relpscost", scip_params),
-        ClassicSolverCustomBranching("best_ca", scip_params),
     ]
+
+    solvers.extend(custom_solvers)
 
     metrics = ["nnodes", "time", "gap"]
 
     data = evaluate_solvers(solvers, instances, n_instances, metrics)
+
+    r = data.report(
+        SolverEvaluationResults.sg_metric("nnodes", 10),
+        SolverEvaluationResults.sg_metric("time", 1),
+        SolverEvaluationResults.nwins("time"),
+        SolverEvaluationResults.nsolved(),
+        SolverEvaluationResults.auc_score("time"))
+    print(r)
+
     pickle.dump(data, open("data", "wb"))
--- python/train.py
+++ python/train.py
@@ -3,10 +3,11 @@ from mpipool import MPIExecutor
 from mpi4py.MPI import COMM_WORLD
 
 from observer import MyObserver
-# from ucig_generator import UcigGenerator
 from utils import train, create_tool_box
 
 n_instances = 1
+n_tot = 1
+
 
 instances_ca = ecole.instance.CombinatorialAuctionGenerator(n_items=100, n_bids=500)
 instances_ca.seed(12)
@@ -17,15 +18,8 @@ instances_sc.seed(12)
 instances_cfl = ecole.instance.CapacitatedFacilityLocationGenerator(n_customers=100)
 instances_cfl.seed(12)
 
-# instances = FolderInstanceGenerator("./instances/MVC-easy/train")
-# instances.seed(0)
 
-# instances = UcigGenerator("./instances/uc_hetrogeneous_5_1")
-
-instances = [instance for _, instance in zip(range(1), instances_ca)]
-
-#easy_instances = ['/home/simon/Téléchargements/benchmark/irp.mps', '/home/simon/Téléchargements/benchmark/roll3000.mps', '/home/simon/Téléchargements/benchmark/nursesched-sprint02.mps', '/home/simon/Téléchargements/benchmark/swath1.mps', '/home/simon/Téléchargements/benchmark/neos-3083819-nubu.mps', '/home/simon/Téléchargements/benchmark/neos-1445765.mps', '/home/simon/Téléchargements/benchmark/neos-1122047.mps', '/home/simon/Téléchargements/benchmark/beasleyC3.mps', '/home/simon/Téléchargements/benchmark/neos8.mps', '/home/simon/Téléchargements/benchmark/air05.mps', '/home/simon/Téléchargements/benchmark/neos-911970.mps', '/home/simon/Téléchargements/benchmark/neos-860300.mps', '/home/simon/Téléchargements/benchmark/drayage-100-23.mps', '/home/simon/Téléchargements/benchmark/binkar10_1.mps', '/home/simon/Téléchargements/benchmark/pg.mps', '/home/simon/Téléchargements/benchmark/piperout-27.mps', '/home/simon/Téléchargements/benchmark/cbs-cta.mps', '/home/simon/Téléchargements/benchmark/n5-3.mps', '/home/simon/Téléchargements/benchmark/neos-1582420.mps', '/home/simon/Téléchargements/benchmark/mik-250-20-75-4.mps', '/home/simon/Téléchargements/benchmark/timtab1.mps']
-#instances = [ecole.scip.Model.from_file(p) for p in easy_instances]
+instances = [instance for _, instance in zip(range(n_tot), instances_ca)]
 
 scip_params = {
     "limits/time": 60,
@@ -41,13 +35,14 @@ if __name__ == "__main__":
 
     with MPIExecutor() as pool:
         pool.workers_exit()
+
         train(
             pool,
             observer=observer,
             instances=instances,
             n_instances=n_instances,
-            pop_size=2,
-            n_generations=2,
+            pop_size=5,
+            n_generations=20,
             best_individual_path="./individuals/test",
             scip_params=scip_params
         )
