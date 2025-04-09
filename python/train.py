19f84b984ea73cc0414fb15e9a5aa4d839d4db74
Simon Renard <simon.renard@ulb.be>
2025-04-09 14:45:01 +0200

    clean

--- python/train.py
+++ python/train.py
@@ -6,20 +6,22 @@ from observer import MyObserver
 from utils import train, create_tool_box
 
 n_instances = 1
-n_tot = 1
 
 
 instances_ca = ecole.instance.CombinatorialAuctionGenerator(n_items=100, n_bids=500)
 instances_ca.seed(12)
 
-instances_sc = ecole.instance.SetCoverGenerator(n_rows=750, n_cols=1000)
+instances_sc = ecole.instance.SetCoverGenerator(n_rows=500, n_cols=1000)
 instances_sc.seed(12)
 
 instances_cfl = ecole.instance.CapacitatedFacilityLocationGenerator(n_customers=100)
 instances_cfl.seed(12)
 
+instances_mis = ecole.instance.IndependentSetGenerator(500)
+instances_mis.seed(12)
 
-instances = [instance for _, instance in zip(range(n_tot), instances_ca)]
+
+instances = [instance for _, *instances in zip(range(n_instances), instances_ca, instances_sc, instances_cfl, instances_mis) for instance in instances]
 
 scip_params = {
     "limits/time": 60,
@@ -40,9 +42,8 @@ if __name__ == "__main__":
             pool,
             observer=observer,
             instances=instances,
-            n_instances=n_instances,
             pop_size=5,
-            n_generations=20,
+            n_generations=3,
             best_individual_path="./individuals/test",
             scip_params=scip_params
         )
