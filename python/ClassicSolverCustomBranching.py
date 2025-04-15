510aa51623dc5512d0ed5cc42da1480730fb39f2
Simon Renard <simon.renard@ulb.be>
2025-04-15 12:08:53 +0200

    Fix memory leak due to circular references

--- python/ClassicSolverCustomBranching.py
+++ python/ClassicSolverCustomBranching.py
@@ -7,18 +7,19 @@ from boundml.solvers import ClassicSolver
 class ClassicSolverCustomBranching(ClassicSolver):
     def __init__(self, branching_rule_name, scip_params={}):
         sys.path.append("observer_generation/lib")
-        self.module = importlib.import_module(branching_rule_name)
+        module = importlib.import_module(branching_rule_name)
+        self.expression = module.to_str()
         sys.path.remove("observer_generation/lib")
         super().__init__(
             branching_rule_name,
             scip_params,
-            config_function=lambda model: self.module.add_branching(model.to_ptr(False))
+            config_function=lambda model: module.add_branching(model.to_ptr(False))
         )
 
         self.state = (branching_rule_name, scip_params)
 
     def get_expression(self):
-        return self.module.to_str()
+        return self.expression
 
     def __getstate__(self):
         return self.state
